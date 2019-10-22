e23ed2d1278932c37ad75c38cacca46dd3fc56c4
石宇 <shiyu@geyecloud.com>
2019-10-22 17:00:14 +0800

    k->w

--- front/index.js
+++ front/index.js
@@ -1,29 +1,52 @@
 /**
  * 数据量很大时的展示
  * @param value
+ * @param minUnit 最小单位 k / w，默认k
  * @return {string|number}
  */
-function formatterNumber(value) {
+function formatterNumber(value, minUnit) {
   let largeNumber = '';
+  minUnit = minUnit || 'k';
   const showValue = parseInt(value, 10);
-  if (showValue < 1000) {
-    largeNumber = showValue;
-  } else if (showValue >= 1000 && showValue < 1000000) {
-    if (showValue % 1000) {
-      largeNumber = `${(showValue / 1000).toFixed(1)}K`;
+  if (minUnit === 'k') {
+    if (showValue < 1000) {
+      largeNumber = showValue;
+    } else if (showValue >= 1000 && showValue < 1000000) {
+      if (showValue % 1000) {
+        largeNumber = `${(showValue / 1000).toFixed(1)}K`;
+      } else {
+        largeNumber = `${showValue / 1000}K`;
+      }
+    } else if (showValue >= 1000000 && showValue < 1000000000) {
+      if (showValue % 1000000) {
+        largeNumber = `${(showValue / 1000000).toFixed(1)}M`;
+      } else {
+        largeNumber = `${showValue / 1000000}M`;
+      }
     } else {
-      largeNumber = `${showValue / 1000}K`;
+      largeNumber = `${(showValue / 1000000000).toFixed(1)}G`;
     }
-  } else if (showValue >= 1000000 && showValue < 1000000000) {
-    if (showValue % 1000000) {
-      largeNumber = `${(showValue / 1000000).toFixed(1)}M`;
+    return largeNumber;
+  } else if (minUnit === 'w') {
+    if (showValue < 10000) {
+      largeNumber = showValue;
+    } else if (showValue >= 10000 && showValue < 1000000) {
+      if (showValue % 10000) {
+        largeNumber = `${(showValue / 10000).toFixed(1)}W`;
+      } else {
+        largeNumber = `${showValue / 10000}W`;
+      }
+    } else if (showValue >= 1000000 && showValue < 1000000000) {
+      if (showValue % 1000000) {
+        largeNumber = `${(showValue / 1000000).toFixed(1)}M`;
+      } else {
+        largeNumber = `${showValue / 1000000}M`;
+      }
     } else {
-      largeNumber = `${showValue / 1000000}M`;
+      largeNumber = `${(showValue / 1000000000).toFixed(1)}G`;
     }
-  } else {
-    largeNumber = `${(showValue / 1000000000).toFixed(1)}G`;
+    return largeNumber;
   }
-  return largeNumber;
 }
 
 /**
